3d659a1d6e7e027cb1b15190ce5507159d30db53
jindaram-stu <jinsb1999@email.daelim.ac.kr>
2021-05-07 02:48:59 +0900

    commit one

---
+++ JavaScriptSourceCode/0504.js
@@ -0,0 +1,34 @@
+/* let foo = 273;
+let chill = 707;
+
+let str = 'Hi~';
+
+function Product(name, price) {
+    this.name = name;
+    this.price = price;
+}
+ */
+
+/* Product.prototype.print_product = function () {
+    console.log(`${product.name}의 가격은 ${product.price}원 입니다.`);
+} */
+// case 2:
+// 모든 객체가 공유하는 메소드를 만들 수 있다.
+/* Product.prototype .print_all = function() {
+    console.log(`${this.price}원의 가격을 가지고 있는 과일은 ${this.name} 입니다.`);
+}
+
+let product = new Product("복숭아",3000);
+let product2 = new Product("자두",1000);
+
+
+product.print_product();
+product.print_all(); */
+/* product2.print_all(); */
+
+let falseBoolean = '';
+
+if (falseBoolean == false) {
+    console.log("hIS");
+    console.log(falseBoolean);
+}
